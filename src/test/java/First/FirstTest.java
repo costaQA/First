07dc32e9ed57a89824b20ed92292ec662ca09c82
costaQA <biz.zolotarev@gmail.com>
2022-11-03 22:31:42 -0400

    Initial commit

---
+++ src/test/java/First/FirstTest.java
@@ -0,0 +1,40 @@
+package First;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+    static {
+        WebDriverManager.chromedriver().setup();
+    }
+    private WebDriver driver;
+
+    @Test
+    public void testFirst() throws InterruptedException {
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        WebElement text = driver.findElement(By.id("my-text-id"));
+        Thread.sleep(2000);
+        text.sendKeys("Testing Chrome");
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//button")).click();
+        Thread.sleep(4000);
+        String actualText = driver.findElement(By.xpath("//h1")).getText();
+        Assert.assertEquals(actualText, "Form submitted");
+    }
+    @BeforeMethod
+    public void setUp() {
+        driver = new ChromeDriver();
+    }
+    @AfterMethod
+    public void tearDown() {
+        //driver.quit();
+    }
+}
